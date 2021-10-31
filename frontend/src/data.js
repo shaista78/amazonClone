010e8c8df37826dcdcc3119838ca29e72a8b9a84
shaista78 <shaistasulthana78@gmail.com>
2021-10-31 21:30:37 +0530

    project

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data ={
+    products :[
+        {
+            _id:"1",
+            name:"Nike Slim Shirt",
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews:10,
+            description:"high quality product"
+        },
+         {
+            _id:"2",
+            name:"Adiads Slim Shirt",
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:220,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews:10,
+            description:"high quality product"
+        },
+         {
+            _id:"3",
+            name:"Lacoste Slim Shirt",
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:180,
+            brand:'Lacoset',
+            rating: 4.5,
+            numReviews:10,
+            description:"high quality product"
+        },
+         {
+            _id:"4",
+            name:"Nike Slim Shirt",
+            category:'pantss',
+            image:'/images/p4.jpg',
+            price:120,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews:10,
+            description:"high quality product"
+        },
+         {
+            _id:"5",
+            name:"Puma Slim pant",
+            category:'Shirts',
+            image:'/images/p5.jpg',
+            price:120,
+            brand:'puma',
+            rating: 4.5,
+            numReviews:10,
+            description:"high quality product"
+        },
+         {
+            _id:"6",
+            name:"Adidas fit Pant",
+            category:'Pants',
+            image:'/images/p6.jpg',
+            price:139,
+            brand:'Adiads',
+            rating: 4.5,
+            numReviews:10,
+            description:"high quality product"
+        },
+    ]
+}
+export default data;
